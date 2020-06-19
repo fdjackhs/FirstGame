87f95d70ce694d65f2b10efd9eaaf4cf2c5419c5
Maxim <sipak1045@gmail.com>
2020-06-19 16:36:09 +0300

    Start project

---
+++ FirstGame/src/Source.cpp
@@ -0,0 +1,46 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const GLuint WIDTH = 800;
+const GLuint HEIGHT = 600;
+
+int main()
+{
+	if (!glfwInit())
+	{
+		std::cout << "glfwInit() failed" << std::endl;
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "FirstGame", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "glfwCreateWindow failed" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL())
+	{
+		std::cout << "gladLoadGL() failed" << std::endl;
+	}
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
